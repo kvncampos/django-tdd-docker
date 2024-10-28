75014a0f8fc6f21fb6f2215b9e0647637adc3c9a
kvncampos <kvnelpdfw@hotmail.com>
2024-10-27 20:12:50 -0500

    updated ci.yml

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -30,6 +30,18 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = "DENY"
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_REFERRER_POLICY = "same-origin"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Add Whitenoise static files storage configuration
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
